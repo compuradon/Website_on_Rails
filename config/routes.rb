1bfdf61677fdedbfc3b994a264523e10e8d639e8
Thomas Esch <t.esch1@yahoo.com>
2012-05-17 21:01:48 -0400

    Gemfile edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Homepage::Application.routes.draw do
   resources :users
   get "users/index"
-  root :to => 'users'
+  root :to => 'users#index'
 
 
   # The priority is based upon order of creation:
