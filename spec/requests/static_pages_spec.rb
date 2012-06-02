a7d2d0709760e9f13d8a832af3b692af35a32639
Thomas Esch <t.esch1@yahoo.com>
2012-06-01 22:44:22 -0400

    spec changed/icon removed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "StaticPages" do
   
   describe "Home page" do
     
-    it "should have the h1 'Toms Homepage'" do
+    it "should have the h1 'Tom's Homepage'" do
           visit '/static_pages/home'
           page.should have_selector('h1', :text => 'Toms Homepage')
         end
