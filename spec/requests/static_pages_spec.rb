d4ff3a3202030bf92e3f17c96f55efffe8b6d2a7
Thomas Esch <t.esch1@yahoo.com>
2012-06-01 23:26:16 -0400

    rspec testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "StaticPages" do
     
     it "should have the h1 'Tom's Homepage'" do
           visit '/static_pages/home'
-          page.should have_selector('h1', :text => 'Toms Homepage')
+          page.should have_selector('h1', :text => "Tom's Homepage")
         end
 
         it "should have the base title" do
