33403ceb205afd3cea56be874166692a94753af2
Thomas Esch <t.esch1@yahoo.com>
2012-07-07 21:14:38 -0400

    users done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120518003645) do
+ActiveRecord::Schema.define(:version => 20120708010322) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
