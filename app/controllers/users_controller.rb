6c71ee79501a8379d0d3c7eb93d7a1d558f9965d
Thomas Esch <t.esch1@yahoo.com>
2012-07-21 08:03:12 -0400

    Added microposting again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page=> params[:page])
     
   end
 
@@ -64,5 +65,6 @@ class UsersController < ApplicationController
   def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
- 
+
+  
 end
